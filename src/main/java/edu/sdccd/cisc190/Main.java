6e1cfb01d84f945cbb8770976cc54020a699eb19
Edward Burkeen <emburkeen@gmail.com>
2024-02-08 11:23:55 -0800

    initial commit

---
+++ src/main/java/edu/sdccd/cisc190/Main.java
@@ -0,0 +1,20 @@
+package edu.sdccd.cisc190;
+
+public class Main {
+    public static void main(String[] args){
+        int declarationTest;
+
+        int integerDemo = 1;
+        System.out.println(integerDemo);
+        integerDemo++;
+        System.out.println(integerDemo);
+
+        long longDemo = 999999999L;
+        System.out.println(longDemo);
+        System.out.println(longDemo + 200);
+        System.out.println(longDemo - 200);
+        System.out.println(longDemo / 5);
+        System.out.println(longDemo * 25);
+
+    }
+}
